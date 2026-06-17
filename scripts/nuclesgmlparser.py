8c4dd038197b7072fc03870896a68d7184bbddf4
ArtyomKopan <64657781+ArtyomKopan@users.noreply.github.com>
2026-06-17 23:00:05 +0300

    Update nuclesgmlparser.py

--- scripts/nuclesgmlparser.py
+++ scripts/nuclesgmlparser.py
@@ -11,32 +11,18 @@
 # It is free for research and educational purposes.
 
 from sgmllib import SGMLParser
-
-
-class nucle_doc:
-    def __init__(self):
-        self.docattrs = None
-        self.matric = None
-        self.email = None
-        self.nationality = None
-        self.firstLanguage = None
-        self.schoolLanguage = None
-        self.englishTests = None
-        self.paragraphs = []
-        self.annotation = []
-        self.mistakes = []
+from nucle_doc import nucle_doc
 
 
 class nuclesgmlparser(SGMLParser):
     def __init__(self):
-        super().__init__()
+        SGMLParser.__init__(self)
         self.docs = []
-        self.data = []
 
     def reset(self):
         self.docs = []
         self.data = []
-        super().reset()
+        SGMLParser.reset(self)
 
     def unknow_starttag(self, tag, attrs):
         pass
@@ -57,6 +43,7 @@ class nuclesgmlparser(SGMLParser):
     def end_matric(self):
         self.docs[-1].matric = ''.join(self.data)
         self.data = []
+        pass
 
     def start_email(self, attrs):
         pass
@@ -64,6 +51,7 @@ class nuclesgmlparser(SGMLParser):
     def end_email(self):
         self.docs[-1].email = ''.join(self.data)
         self.data = []
+        pass
 
     def start_nationality(self, attrs):
         pass
@@ -71,6 +59,7 @@ class nuclesgmlparser(SGMLParser):
     def end_nationality(self):
         self.docs[-1].nationality = ''.join(self.data)
         self.data = []
+        pass
 
     def start_first_language(self, attrs):
         pass
@@ -78,6 +67,7 @@ class nuclesgmlparser(SGMLParser):
     def end_first_language(self):
         self.docs[-1].firstLanguage = ''.join(self.data)
         self.data = []
+        pass
 
     def start_school_language(self, attrs):
         pass
@@ -85,6 +75,7 @@ class nuclesgmlparser(SGMLParser):
     def end_school_language(self):
         self.docs[-1].schoolLanguage = ''.join(self.data)
         self.data = []
+        pass
 
     def start_english_tests(self, attrs):
         pass
@@ -92,10 +83,12 @@ class nuclesgmlparser(SGMLParser):
     def end_english_tests(self):
         self.docs[-1].englishTests = ''.join(self.data)
         self.data = []
+        pass
+
 
     def start_text(self, attrs):
         pass
-
+    
     def end_text(self):
         pass
 
@@ -105,6 +98,8 @@ class nuclesgmlparser(SGMLParser):
     def end_title(self):
         self.docs[-1].paragraphs.append(''.join(self.data))
         self.data = []
+        pass
+
 
     def start_p(self, attrs):
         pass
@@ -112,6 +107,8 @@ class nuclesgmlparser(SGMLParser):
     def end_p(self):
         self.docs[-1].paragraphs.append(''.join(self.data))
         self.data = []
+        pass
+
 
     def start_annotation(self, attrs):
         self.docs[-1].annotation.append(attrs)
@@ -124,9 +121,10 @@ class nuclesgmlparser(SGMLParser):
         for t in attrs:
             d[t[0]] = int(t[1])
         self.docs[-1].mistakes.append(d)
+        pass 
 
     def end_mistake(self):
-        pass
+        pass 
 
     def start_type(self, attrs):
         pass
@@ -146,23 +144,25 @@ class nuclesgmlparser(SGMLParser):
         pass
 
     def end_comment(self):
-        self.docs[-1].mistakes[-1]['comment'] = ''.join(self.data)
+        self.docs[-1].mistakes[-1]['comment'] = ''.join( self.data)
         self.data = []
 
+
     def handle_charref(self, ref):
-        self.data.append(f'&{ref}')
+        self.data.append('&' + ref)
 
     def handle_entityref(self, ref):
-        self.data.append(f'&{ref}')
+        self.data.append('&' + ref)
 
     def handle_data(self, text):
-        text = text.strip()
-        if not text:
+        if  text.strip() == '':
             self.data.append('')
             return
+        else:
+            if text.startswith('\n'):
+                text = text[1:]
+            if text.endswith('\n'):
+                text = text[:-1]
+            self.data.append(text)
 
-        if text.startswith('\n'):
-            text = text[1:]
-        if text.endswith('\n'):
-            text = text[:-1]
-        self.data.append(text)
+
